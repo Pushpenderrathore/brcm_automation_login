c0c9492c69c99537e131ee58cb2092a628b2b1f1
Pushpenderrathore <pushpendersingh046@gmai.com>
2025-05-02 11:05:26 +0530

    brcm_Automation

---
+++ autologin.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Portal URL
+URL="http://10.10.10.1:8090/login.xml"
+
+# List of credentials (username:password)
+CREDENTIALS=(
+  "cse4956:495665"
+  "cse4957:495775"
+  "cse4958:495885"
+  # Add more here
+)
+
+# Try each credential until login succeeds
+for cred in "${CREDENTIALS[@]}"; do
+  USERNAME="${cred%%:*}"
+  PASSWORD="${cred##*:}"
+
+  RESPONSE=$(curl -s -X POST "$URL" \
+    -d "mode=191" \
+    -d "username=$USERNAME" \
+    -d "password=$PASSWORD" \
+    -d "a=$(date +%s)" \
+    -d "producttype=0")
+
+  if echo "$RESPONSE" | grep -q "LIVE"; then
+    echo "Logged in with $USERNAME"
+    break
+  else
+    echo "Failed with $USERNAME, trying next..."
+  fi
+done
