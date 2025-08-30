c9bcad7431f28aea9dde6bdbbc64bf62e5b93ea6
root <root@localhost.localdomain>
2025-08-30 11:41:29 -0400

    modified

--- autologin.sh
+++ autologin.sh
@@ -2,7 +2,6 @@
 
 # Connect to network in case network is not saved
 nmcli device wifi connect "Bhabha 1"
-nmcli device wifi connect "Wavion-1"
 
 # Portal URL
 URL="http://10.10.10.1:8090/login.xml"
